145ed3337c07b2e4f90a856c4218b8163686cf01
likemike12345 <michael.manyonganise@gmail.com>
2023-02-10 17:33:49 +0000

    working API's for the weather and joke section with some CSS for the weather section

---
+++ JOKEscript.js
@@ -0,0 +1,28 @@
+const jokeButton = document.querySelector(".getJoke")
+const jokeHolder = document.querySelector(".joke")
+
+
+async function fetchJoke() {
+  const response = await fetch("http://icanhazdadjoke.com", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const data = response.json();
+  return data;
+}
+
+async function handleClick() {
+  const { joke } = await fetchJoke();
+  jokeHolder.textContent = joke;
+}
+
+jokeButton.addEventListener("click", handleClick);
+
+
+
+
+
+
+
+
